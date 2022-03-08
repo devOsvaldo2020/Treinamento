0c09cc09d53a44e6221df549eb93efbbf1b04ff2
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-03-08 11:43:55 -0300

    adicionando o demo2 seguindo aula da dio ainda de .net com c#

---
+++ dio/carrefor/dotnet/demo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+public class Program{
+
+    static int Adicionar20(int x){
+        return x + 20;
+    }
+    public static void Main(){
+        int a = 2; 
+        a = Adicionar20(a);
+        WriteLine($"O valor da variável é {a}");
+    }
+}
